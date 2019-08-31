6e965be2c6a0425a5597df13fb84f9be8d4d7204
Kreddyy <52068965+Kreddyy@users.noreply.github.com>
2019-08-31 13:55:15 +0200

    Add files via upload

---
+++ esx_phonestore/locales/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+  ['shop'] = 'Telefon butik',
+  ['shops'] = 'telefon butik',
+  ['press_menu'] = 'Tryk ~INPUT_CONTEXT~ for at se ~y~butikken~s~.',
+  ['shop_item'] = '%s kr',
+  ['bought'] = 'Du har lige købt ~y~%sx~s~ ~b~%s~s~ for ~r~DKK%s~s~',
+  ['not_enough'] = 'Du har ikke ~r~nok~s~ penge, du ~y~mangler~s~ ~r~%s kr~s~!',
+  ['player_cannot_hold'] = 'du har ~r~ikke~s~ nok ~y~plads~s~ i din rygsæk!',
+  ['shop_confirm'] = 'køb %sx %s for DKK%s?',
+  ['no'] = 'nej',
+  ['yes'] = 'ja',
+}
